f69a7a8cff9e4041a0a6e99d1165d8d7b1dbb074
Sergey Alpeev <salpeev@briskmobile.com>
2013-09-27 17:57:28 +0300

    Listing 2.4

---
+++ LearnCpp/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  LearnCpp
+//
+//  Created by Sergey Alpeev on 19.06.13.
+//  Copyright (c) 2013 Sergey Alpeev. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+
+
+
+int main()
+{
+    using namespace std;
+    
+    double area;
+    cout << "Enter the floor area, in the square feet, of your home: ";
+    cin >> area;
+    
+    double side;
+    side = sqrt(area);
+    cout << "That's the equivalent of the square " << side << " feet to the side" << endl;
+    cout << "How fascinating!" << endl;
+    
+    return 0;
+}
