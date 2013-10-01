994465f4edf9b8f9c780ec8543388d93f44f4366
Sergey Alpeev <salpeev@briskmobile.com>
2013-10-01 12:35:30 +0300

    Listing 3.2

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -11,6 +11,10 @@
 
 
 
+#define ZERO 0
+
+
+
 int StoneToLb(int sts);
 
 
@@ -19,25 +23,25 @@ int main()
 {
     using namespace std;
     
-    short n_short = SHRT_MAX;
-    int n_int = INT_MAX;
-    long n_long(LONG_MAX);
-    long long n_llong(LONG_LONG_MAX);
+    short sam = SHRT_MAX;
+    unsigned short sue = sam;
+    cout << "sam: " << sam << "   sue: " << sue << endl;
     
-    cout << "short width: " << sizeof(short) << endl;
-    cout << "int width: " << sizeof(int) << endl;
-    cout << "long width: " << sizeof(n_long) << endl;
-    cout << "long long width: " << sizeof(n_llong) << endl;
-    cout << endl;
+    cout << "Add 1" << endl;
     
-    cout << "max short: " << n_short << endl;
-    cout << "max int: " << n_int << endl;
-    cout << "max long: " << n_long << endl;
-    cout << "max long long: " << n_llong << endl;
-    cout << endl;
+    sam = sam + 1;
+    sue = sue + 1;
+    cout << "sam: " << sam << "   sue: " << sue << endl;
     
-    cout << "min int: " << INT_MIN << endl;
-    cout << "bits per byte: " << CHAR_BIT << endl;
+    sam = ZERO;
+    sue = ZERO;
+    cout << "sam: " << sam << "   sue: " << sue << endl;
+    
+    cout << "Take 1" << endl;
+    
+    sam = sam - 1;
+    sue = sue - 1;
+    cout << "sam: " << sam << "   sue: " << sue << endl;
     
     return 0;
 }
