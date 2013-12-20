d75601505d901b83387cfc2809e039bf3d387874
Sergey Alpeev <salpeev@briskmobile.com>
2013-12-20 12:53:56 +0200

    Listing 3.8

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -7,7 +7,6 @@
 //
 
 #include <iostream>
-#include <cfloat>
 
 
 
@@ -15,7 +14,19 @@ int main()
 {
     using namespace std;
     
-    cout << DBL_DIG << endl;
+    cout.setf(ios_base::fixed, ios_base::floatfield);
+    
+    float tub = 10.0 / 3.0;
+    double mint = 10.0 / 3.0;
+    const float million = 1.0e6;
+    
+    cout << "tub = " << tub;
+    cout << ", a million tubs = " << million * tub;
+    cout << ",\nand ten million tubs = ";
+    cout << 10 * million * tub << endl;
+    
+    cout << "mint = " << mint << " and a million mints = ";
+    cout << million * mint << endl;
     
     return 0;
 }
