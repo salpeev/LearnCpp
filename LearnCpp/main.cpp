0004344abb872387216918080dd0c44cd12db91f
Sergey Alpeev <salpeev@briskmobile.com>
2013-12-20 13:04:15 +0200

    Listing 3.9

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -16,17 +16,11 @@ int main()
     
     cout.setf(ios_base::fixed, ios_base::floatfield);
     
-    float tub = 10.0 / 3.0;
-    double mint = 10.0 / 3.0;
-    const float million = 1.0e6;
+    float a = 2.34E+22f;
+    float b = a + 1.0f;
     
-    cout << "tub = " << tub;
-    cout << ", a million tubs = " << million * tub;
-    cout << ",\nand ten million tubs = ";
-    cout << 10 * million * tub << endl;
-    
-    cout << "mint = " << mint << " and a million mints = ";
-    cout << million * mint << endl;
+    cout << "a = " << a << endl;
+    cout << "(b - a) = " << (b - a) << endl;
     
     return 0;
 }
