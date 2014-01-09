4cab5c677f5b7713de5485a5267267a6b7b39b98
Sergey Alpeev <salpeev@briskmobile.com>
2014-01-09 18:33:52 +0200

    The address of an array

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -15,27 +15,12 @@ int main()
     using namespace std;
     
     double wages[3] = {10000.0, 20000.0, 30000.0};
-    short stacks[3] = {3, 2, 1};
     
-    double *pw = wages;
-    short *ps = &stacks[0];
+    cout << wages << endl;
+    cout << &wages << endl;
     
-    cout << "pw: " << pw << "\t*pw: " << *pw << endl;
-    pw++;
-    cout << "pw: " << pw << "\t*pw: " << *pw << endl;
-    cout << endl;
-    
-    cout << "ps: " << ps << "\t*ps: " << *ps << endl;
-    ps++;
-    cout << "ps: " << ps << "\t*ps: " << *ps << endl;
-    cout << endl;
-    
-    cout << "stacks[0]: " << stacks[0] << "\tstacks[1]: " << stacks[1] << endl;
-    cout << "*stacks: " << *stacks << "\t*(stacks + 1): " << *(stacks + 1) << endl;
-    cout << endl;
-    
-    cout << "sizeof(wages): " << sizeof(wages) << endl;
-    cout << "sizeof(pw): " << sizeof(pw) << endl;
+    cout << "wages: " << wages << "\t(wages + 1): " << (wages + 1) << endl;         // + sizeof(double) bytes
+    cout << "&wages: " << &wages << "\t(&wages + 1): " << (&wages + 1) << endl;     // + (sizeof(double) * 3) bytes
     
     return 0;
 }
