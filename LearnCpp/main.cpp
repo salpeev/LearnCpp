29fe8ba58d0d7f8ca53a83c7e27cf941277503f8
Sergey Alpeev <salpeev@briskmobile.com>
2014-01-09 17:49:22 +0200

    Listing 4.18

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -14,27 +14,23 @@ int main()
 {
     using namespace std;
     
-    cout.setf(ios_base::fixed, ios_base::floatfield);
+    double *p3 = new double[3];
+    p3[0] = 0.2;
+    p3[1] = 0.5;
+    p3[2] = 0.8;
     
-    int nights = 1001;
-    int *pt = new int;
-    *pt = 1001;
-    
-    cout << "&nights: " << &nights << "\t\tnights: " << nights << endl;
-    cout << "pt: "<< pt << "\t\t\t\t*pt: " << *pt << endl;
+    cout << "p3[1]: " << p3[1] << endl;
     cout << endl;
     
-    double *pd = new double;
-    *pd = 10000001.0;
-    
-    cout << "pd: " << pd << "\t\t\t\t*pd: " << *pd << endl;
+    p3 = p3 + 1;
+    cout << "p3[0]: " << p3[0] << endl;
+    cout << "p3[1]: " << p3[1] << endl;
     cout << endl;
     
-    cout << "sizeof(pt): " << sizeof(pt) << "\t\tsizeof(*pt)" << sizeof(*pt) << endl;
-    cout << "sizeof(pd): " << sizeof(pd) << "\t\tsizeof(*pd)" << sizeof(*pd) << endl;
+    p3 = p3 - 1;
+    cout << "p3[0]: " << p3[0] << endl;
     
-    delete pt;
-    delete pd;
+    delete [] p3;
     
     return 0;
 }
