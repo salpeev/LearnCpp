e1b84807ff332a6bab7e06234656f2f709ae478e
Sergey Alpeev <salpeev@briskmobile.com>
2014-01-09 19:00:43 +0200

    Listing 4.20

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include <cstring>
 
 
 
@@ -14,13 +15,35 @@ int main()
 {
     using namespace std;
     
-    double wages[3] = {10000.0, 20000.0, 30000.0};
+    char animal[20] = "bear";
+    const char *bird = "wren";
+    char *ps;
     
-    cout << wages << endl;
-    cout << &wages << endl;
+    cout << animal << endl;
+    cout << bird << endl;
+    cout << endl;
     
-    cout << "wages: " << wages << "\t(wages + 1): " << (wages + 1) << endl;         // + sizeof(double) bytes
-    cout << "&wages: " << &wages << "\t(&wages + 1): " << (&wages + 1) << endl;     // + (sizeof(double) * 3) bytes
+    cout << "Enter an animal: ";
+    cin >> animal;
+    
+    ps = animal;
+    cout << ps << endl;
+    cout << endl;
+    
+    cout << "Before strcpy(): " << endl;
+    cout << animal << " at " << (int *)animal << endl;
+    cout << ps << " at " << (int *)ps << endl;
+    cout << endl;
+    
+    ps = new char[strlen(animal) + 1];
+    strcpy(ps, animal);
+    
+    cout << "After strcpy(): " << endl;
+    cout << animal << " at " << (int *)animal << endl;
+    cout << ps << " at " << (int *)ps << endl;
+    cout << endl;
+    
+    delete [] ps;
     
     return 0;
 }
