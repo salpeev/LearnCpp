705517daa7b422828df64172c823794bc3121abb
Sergey Alpeev <salpeev@briskmobile.com>
2014-04-29 15:17:09 +0300

    Listing 6.1

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -10,38 +10,26 @@
 
 
 
-const int Cities = 5;
-const int Years = 4;
-
-
-
 int main() {
-    using namespace std;
+    using std::cin;
+    using std::cout;
+    using std::endl;
     
-    const char * cities[Cities] = {
-        "Gribble City",
-        "Gribbletown",
-        "New Gribble",
-        "San Gribble",
-        "Gribble Vista"
-    };
+    char ch;
+    int spaces = 0;
+    int total = 0;
     
-    int maxtemps[Years][Cities] = {
-        {96, 100, 87, 101, 105},
-        {96, 98, 91, 107, 104},
-        {97, 101, 93, 108, 107},
-        {98, 103, 95, 109, 108}
-    };
-    
-    for (int city = 0; city < Cities; city++) {
-        cout << cities[city] << ":\t";
-        
-        for (int year = 0; year < Years; year++) {
-            cout << maxtemps[year][city] << "\t";
+    cin.get(ch);
+    while (ch != '.') {
+        if (ch == ' ') {
+            spaces++;
         }
-        
-        cout << endl;
+        total++;
+        cin.get(ch);
     }
+    
+    cout << "Spaces: " << spaces << endl;
+    cout << "Total: " << total << endl;
     
     return 0;
 }
