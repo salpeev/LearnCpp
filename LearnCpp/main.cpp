30e7b7dbfde74eea718c27eb60b699dceead459a
Sergey Alpeev <salpeev@Brisks-Mac-mini.local>
2014-04-30 15:19:39 +0300

    Listing 6.5

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -10,20 +10,48 @@
 
 
 
+const int ArSize = 6;
+
+
+
 int main() {
     using namespace std;
     
-    cout << "Format your hard disk? (y/n): ";
+    float naaq[ArSize];
+    cout << "Enter the NAAQs (New Age Awareness Quotients) ";
+    cout << "of\nyour neighbors. Program terminates ";
+    cout << "when you make\n" << ArSize << " entries ";
+    cout << "or enter a negative value.\n";
     
-    char ch;
-    cin >> ch;
+    int i = 0;
+    float temp;
     
-    if (ch == 'y' || ch == 'Y') {
-        cout << "You were warned!" << endl;
-    } else if (ch == 'n' || ch == 'N') {
-        cout << "A wise choice." << endl;
+    cout << "First value: ";
+    cin >> temp;
+    
+    while (i < ArSize && temp >= 0) {
+        naaq[i] = temp;
+        i++;
+        if (i < ArSize) {
+            cout << "Next value: ";
+            cin >> temp;
+        }
+    }
+    
+    if (i == 0) {
+        cout << "No data, bye.\n";
     } else {
-        cout << "Wrong input." << endl;
+        cout << "Enter your NAAQ: ";
+        float you;
+        cin >> you;
+        int count = 0;
+        for (int j = 0; j < i; j++) {
+            if (naaq[j] > you) {
+                count++;
+            }
+        }
+        cout << count << " of your neighbors have greater awareness of\n";
+        cout << "the New Age than do.\n";
     }
     
     return 0;
