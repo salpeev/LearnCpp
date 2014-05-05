e259e3b2ffa1f9a80eef55998776ace5fd6b8071
Sergey Alpeev <salpeev@Brisks-Mac-mini.local>
2014-05-05 11:24:18 +0300

    Listing 6.9

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -7,39 +7,20 @@
 //
 
 #include <iostream>
-#include <cctype>
 
 
 
 int main() {
     using namespace std;
     
-    cout << "Enter text for analysis and type @ to terminate input.\n";
+    cout << "Enter two integers: ";
+    int a;
+    int b;
+    cin >> a >> b;
     
-    char ch;
-    int whitespace = 0;
-    int digits = 0;
-    int chars = 0;
-    int punct = 0;
-    int others = 0;
-    
-    cin.get(ch);
-    while (ch != '@') {
-        if (isalpha(ch)) {
-            chars++;
-        } else if (isspace(ch)) {
-            whitespace++;
-        } else if (isdigit(ch)) {
-            digits++;
-        } else if (ispunct(ch)) {
-            punct++;
-        } else {
-            others++;
-        }
-        cin.get(ch);
-    }
-    
-    cout << chars << " letters, " << whitespace << " whitespace, " << digits << " digits, " << punct << " punctuations, " << others << " others.\n";
+    cout << "The larger of " << a << " and " << b;
+    int c = (a > b) ? a : b;
+    cout << " is " << c << endl;
     
     return 0;
 }
