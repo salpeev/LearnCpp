6b4a4c4a733139ab6a429a2783605608e886c7bc
Sergey Alpeev <salpeev@GFL-1894.local>
2014-05-08 13:16:59 +0300

    Listing 7.1

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -7,48 +7,26 @@
 //
 
 #include <iostream>
-#include <fstream>
-#include <cstdlib>
+
+
+
+void simple();
 
 
 
 int main() {
     using namespace std;
     
-    ifstream fin;
-    fin.open("/Users/salpeev/Documents/Projects/LearnCpp/LearnCpp/numbers.txt");
-    
-    if (!fin.is_open()) {
-        cout << "Couldn't open the file. Terminating...\n";
-        exit(EXIT_FAILURE);
-    }
-    
-    double value;
-    double sum = 0.0;
-    int count = 0;
-    
-    while (fin >> value) {
-        count++;
-        sum += value;
-    }
-    
-    if (fin.eof()) {
-        cout << "End of file reached.\n";
-    } else if (fin.fail()) {
-        cout << "Input terminated by data mismatch.\n";
-    } else {
-        cout << "Input terminated by unknown reason.\n";
-    }
-    
-    if (count == 0) {
-        cout << "No data processed.\n";
-    } else {
-        cout << "Items read: " << count << endl;
-        cout << "Sum: " << sum << endl;
-        cout << "Average: " << sum / count << endl;
-    }
-    
-    fin.close();
+    cout << "main() will call the simple() function.\n";
+    simple();
+    cout << "main() is finished with the simple().\n";
     
     return 0;
+}
+
+
+
+void simple() {
+    using namespace std;
+    cout << "I'm but a simple function.\n";
 }
