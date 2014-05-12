bd14269a058661632263ed12506a68816e95e6b8
Sergey Alpeev <salpeev@GFL-1894.local>
2014-05-12 14:46:15 +0300

    Listing 7.14

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -7,52 +7,39 @@
 //
 
 #include <iostream>
-#include <cmath>
+#include <string>
 
 using namespace std;
 
 
 
-struct polar {
-    double distance;
-    double angle;
-};
-
-struct rect {
-    double x;
-    double y;
-};
+const int SIZE = 5;
 
 
 
-void rect_to_polar(const rect *pxy, polar *pda);
-void show_polar(const polar *pda);
+void display(const string sa[], int n);
 
 
 
 int main() {
-    rect rplace;
-    polar pplace;
+    string list[SIZE];
     
-    cout << "Enter the x and y values: ";
-    while (cin >> rplace.x >> rplace.y) {
-        rect_to_polar(&rplace, &pplace);
-        show_polar(&pplace);
-        
-        cout << "Next two numbers (q to quit): ";
+    cout << "Enter your " << SIZE << " favorite astronomical sights:\n";
+    for (int i = 0; i < SIZE; i++) {
+        cout << i + 1 << ": ";
+        getline(cin, list[i]);
     }
+    
+    cout << "Your list:\n";
+    display(list, SIZE);
     
     return 0;
 }
 
 
 
-void rect_to_polar(const rect *pxy, polar *pda) {
-    pda->distance = sqrt(pxy->x * pxy->x + pxy->y * pxy->y);
-    pda->angle = atan2(pxy->y, pxy->x);
-}
-
-void show_polar(const polar *pda) {
-    double degInRad = 180.0 / M_PI;
-    cout << "distance = " << pda->distance << ", angle = " << pda->angle * degInRad << " degrees.\n";
+void display(const string sa[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << i + 1 << ": " << sa[i] << endl;
+    }
 }
