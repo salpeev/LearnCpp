5457775e15222b93893951986d14c43fee0cc11d
Sergey Alpeev <salpeev@GFL-1894.local>
2014-05-12 15:54:39 +0300

    Listing 7.17

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -12,24 +12,52 @@ using namespace std;
 
 
 
-void countdown(int n);
+const int Len = 66;
+const int Divs = 6;
+
+
+
+void subdivide(char ar[], int low, int high, int level);
 
 
 
 int main() {
-    countdown(4);
+    char ruler[Len];
+    
+    for (int i = 1; i < (Len - 2); i++) {
+        ruler[i] = ' ';
+    }
+    
+    ruler[Len - 1] = '\0';
+    
+    int max = Len - 2;
+    int min = 0;
+    ruler[min] = ruler[max] = '|';
+    
+    cout << ruler << endl;
+    
+    for (int i = 1; i <= Divs; i++) {
+        subdivide(ruler, min, max, i);
+        cout << ruler << endl;
+        
+        for (int j = 1; j < (Len - 2); j++) {
+            ruler[j] = ' ';
+        }
+    }
     
     return 0;
 }
 
 
 
-void countdown(int n) {
-    cout << "Counting down... " << n << "   at " << &n << endl;
-    
-    if (n > 0) {
-        countdown(n - 1);
+void subdivide(char ar[], int low, int high, int level) {
+    if (level == 0) {
+        return;
     }
     
-    cout << n << ": Kaboom!" << "           at " << &n << endl;
+    int mid = (high + low) / 2;
+    ar[mid] = '|';
+    
+    subdivide(ar, low, mid, level - 1);
+    subdivide(ar, mid, high, level - 1);
 }
