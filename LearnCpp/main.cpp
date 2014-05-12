51bb7ca2d613157a22945eb6e7fcfd93f978090d
Sergey Alpeev <salpeev@GFL-1894.local>
2014-05-12 15:36:27 +0300

    Listing 7.16

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -7,48 +7,29 @@
 //
 
 #include <iostream>
-#include <array>
-#include <string>
 
 using namespace std;
 
 
 
-const int Seasons = 4;
-const array<string, Seasons> Snames = {"Spring", "Summer", "Fall", "Winter"};
-
-
-
-void fill(array<double, Seasons> *pa);
-void show(array<double, Seasons> da);
+void countdown(int n);
 
 
 
 int main() {
-    array<double, Seasons> expenses;
-    fill(&expenses);
-    show(expenses);
+    countdown(4);
     
     return 0;
 }
 
 
 
-void fill(array<double, Seasons> *pa) {
-    for (int i = 0; i < Seasons; i++) {
-        cout << "Enter " << Snames[i] << " expenses: ";
-        cin >> (*pa)[i];
-    }
-}
-
-void show(array<double, Seasons> da) {
-    cout << "\nEXPENSES\n";
+void countdown(int n) {
+    cout << "Counting down... " << n << endl;
     
-    double total = 0.0;
-    for (int i = 0; i < Seasons; i++) {
-        cout << Snames[i] << ": $" << da[i] << endl;
-        total += da[i];
+    if (n > 0) {
+        countdown(n - 1);
     }
     
-    cout << "Total expenses: " << total << endl;
+    cout << n << ": Kaboom!\n";
 }
