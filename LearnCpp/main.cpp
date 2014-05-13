4b68a00b9311c5dac7ab8c644d4b599d8b975d06
Sergey Alpeev <salpeev@GFL-1894.local>
2014-05-13 16:44:59 +0300

    rvalue reference

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -7,33 +7,15 @@
 //
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 
 
-double cube(double a);
-double refcube(double &ra);
-
-
-
 int main() {
-    double x = 3.0;
-    
-    cout << cube(x) << " = cube of " << x << endl;
-    cout << refcube(x) << " = cube of " << x << endl;
+    double &&rref = sqrt(36);
+    cout << rref << endl;
     
     return 0;
-}
-
-
-
-double cube(double a) {
-    a *= a * a;
-    return a;
-}
-
-double refcube(double &ra) {
-    ra *= ra * ra;
-    return ra;
 }
