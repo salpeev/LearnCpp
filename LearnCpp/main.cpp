c798365757824b800e3ecb83aa38ac9979823891
Sergey Alpeev <salpeev@GFL-1894.local>
2014-05-13 15:04:44 +0300

    Listing 8.3

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -19,5 +19,11 @@ int main() {
     cout << "rats = " << rats << ", rodents = " << rodents << endl;
     cout << "rats address = " << &rats << ", rodents address = " << &rodents << endl;
     
+    int bunnies = 50;
+    rodents = bunnies;
+    
+    cout << "bunnies = " << bunnies << ", rats = " << rats << ", rodents = " << rodents << endl;
+    cout << "bunnies address = " << &bunnies << ", rodents address = " << &rodents << endl;
+    
     return 0;
 }
