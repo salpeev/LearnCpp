65749d6f2c8a53c2d3947140a415f3cc18766b58
Sergey Alpeev <salpeev@GFL-1894.local>
2014-05-14 15:49:06 +0300

    Listing 8.12

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -12,9 +12,17 @@ using namespace std;
 
 
 
+const int Lim = 8;
+
+
+
 template <typename T>
 void Swap(T &a, T &b);
 
+template <typename T>
+void Swap(T *a, T *b, int n);
+
+void Show(int a[]);
 
 
 int main() {
@@ -26,12 +34,17 @@ int main() {
     cout << "Now i, j = " << i << ", " << j << ".\n";
     cout << endl;
     
-    double x = 24.5;
-    double y = 81.7;
+    int d1[Lim] = {0, 7, 0, 4, 1, 7, 7, 6};
+    int d2[Lim] = {0, 7, 2, 0, 1, 9, 6, 9};
+    cout << "Original arrays:\n";
+    Show(d1);
+    Show(d2);
     
-    cout << "x, y = " << x << ", " << y << ".\n";
-    Swap(x, y);
-    cout << "Now x, y = " << x << ", " << y << ".\n";
+    Swap(d1, d2, Lim);
+    
+    cout << "Swapped arrays:\n";
+    Show(d1);
+    Show(d2);
     
     return 0;
 }
@@ -45,3 +58,22 @@ void Swap(T &a, T &b) {
     a = b;
     b = temp;
 }
+
+template <typename T>
+void Swap(T *a, T *b, int n) {
+    for (int i = 0; i < n; i++) {
+        T temp = a[i];
+        a[i] = b[i];
+        b[i] = temp;
+    }
+}
+
+void Show(int a[]) {
+    cout << a[0] << a[1] << "/";
+    cout << a[2] << a[3] << "/";
+    for (int i = 4; i < Lim; i++) {
+        cout << a[i];
+    }
+    cout << endl;
+}
+
