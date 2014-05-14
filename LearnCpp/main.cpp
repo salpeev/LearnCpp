20d96b32f2488dbbe6c77a2dc082e43a5961501e
Sergey Alpeev <salpeev@GFL-1894.local>
2014-05-14 14:45:17 +0300

    rvalue reference

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -12,46 +12,18 @@ using namespace std;
 
 
 
-const int ArSize = 80;
-
-
-
-char * left(const char *str, int n = 1);
+void test(double &&r);
 
 
 
 int main() {
-    char sample[ArSize];
-    cout << "Enter a string: ";
-    cin.getline(sample, ArSize);
-    
-    char *ps = left(sample, 4);
-    cout << ps << endl;
-    delete [] ps;
-    
-    ps = left(sample);
-    cout << ps << endl;
-    delete [] ps;
+    test(10 + 5);
     
     return 0;
 }
 
 
 
-char * left(const char *str, int n) {
-    if (n < 0) {
-        n = 0;
-    }
-    
-    char *p = new char[n + 1];
-    
-    int i;
-    for (i = 0; i < n && str[i]; i++) {
-        p[i] = str[i];
-    }
-    while (i <= n) {
-        p[i++] = '\0';
-    }
-    
-    return p;
+void test(double &&r) {
+    r = 10;
 }
