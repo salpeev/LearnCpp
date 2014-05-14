569d1a7fb03db9c52dd322fe3087b5b13667c77c
Sergey Alpeev <salpeev@GFL-1894.local>
2014-05-14 14:03:25 +0300

    Listing 8.9

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -7,68 +7,51 @@
 //
 
 #include <iostream>
-#include <fstream>
-#include <cstdlib>
 
 using namespace std;
 
 
 
-const int LIMIT = 5;
+const int ArSize = 80;
 
 
 
-void file_it(ostream &os, double fo, const double fe[], int n);
+char * left(const char *str, int n = 1);
 
 
 
 int main() {
-    ofstream fout;
-    const char *fn = "/Users/salpeev/Documents/Projects/LearnCpp/LearnCpp/ep-data.txt";
+    char sample[ArSize];
+    cout << "Enter a string: ";
+    cin.getline(sample, ArSize);
     
-    fout.open(fn);
-    if (!fout.is_open()) {
-        cout << "Can't open: " << fn << endl;
-        exit(EXIT_FAILURE);
-    }
+    char *ps = left(sample, 4);
+    cout << ps << endl;
+    delete [] ps;
     
-    double objective;
-    cout << "Enter the focal length of your telescope objective in mm: ";
-    cin >> objective;
-    
-    double eps[LIMIT];
-    cout << "Enter the focal length, in mm, of " << LIMIT << " eyepieces:\n";
-    for (int i = 0; i < LIMIT; i++) {
-        cout << "Eyepiece #" << (i + 1) << ": ";
-        cin >> eps[i];
-    }
-    
-    file_it(cout, objective, eps, LIMIT);
-    file_it(fout, objective, eps, LIMIT);
+    ps = left(sample);
+    cout << ps << endl;
+    delete [] ps;
     
     return 0;
 }
 
 
 
-void file_it(ostream &os, double fo, const double fe[], int n) {
-    ios_base::fmtflags initial = os.setf(ios_base::fixed);
-    os.precision(0);
-    
-    os << "Focal length of objective: " << fo << " mm\n";
-    os.setf(ios_base::showpoint);
-    os.precision(1);
-    os.width(12);
-    os << "f.1. eyepiece";
-    os.width(15);
-    os << "magnification" << endl;
-    
-    for (int i = 0; i < n; i++) {
-        os.width(12);
-        os << fe[i];
-        os.width(15);
-        os << int(fo / fe[i] + 0.5) << endl;
+char * left(const char *str, int n) {
+    if (n < 0) {
+        n = 0;
     }
     
-    os.setf(initial);
+    char *p = new char[n + 1];
+    
+    int i;
+    for (i = 0; i < n && str[i]; i++) {
+        p[i] = str[i];
+    }
+    while (i <= n) {
+        p[i++] = '\0';
+    }
+    
+    return p;
 }
