57e781b4d6d69bf254f83996ca9f7fb70b4066e5
Sergey Alpeev <salpeev@GFL-1894.local>
2014-05-14 15:36:43 +0300

    Listing 8.11

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -12,64 +12,36 @@ using namespace std;
 
 
 
-unsigned long left(unsigned long num, unsigned int ct);
-char * left(const char *str, int n = 1);
+template <typename T>
+void Swap(T &a, T &b);
 
 
 
 int main() {
-    const char *trip = "Hawaii!!";
-    unsigned long n = 12345678;
+    int i = 10;
+    int j = 20;
     
-    for (int i = 1; i < 10; i++) {
-        cout << left(n, i) << endl;
-        char *temp = left(trip, i);
-        cout << temp << endl;
-        delete [] temp;
-    }
+    cout << "i, j = " << i << ", " << j << ".\n";
+    Swap(i, j);
+    cout << "Now i, j = " << i << ", " << j << ".\n";
+    cout << endl;
+    
+    double x = 24.5;
+    double y = 81.7;
+    
+    cout << "x, y = " << x << ", " << y << ".\n";
+    Swap(x, y);
+    cout << "Now x, y = " << x << ", " << y << ".\n";
     
     return 0;
 }
 
 
 
-unsigned long left(unsigned long num, unsigned int ct) {
-    unsigned int digits = 1;
-    unsigned long n = num;
-    
-    if (ct == 0 || num == 0) {
-        return 0;
-    }
-    
-    while (n /= 10) {
-        digits++;
-    }
-    
-    if (digits > ct) {
-        ct = digits - ct;
-        while (ct--) {
-            num /= 10;
-        }
-        return num;
-    } else {
-        return num;
-    }
-}
-
-char * left(const char *str, int n) {
-    if (n < 0) {
-        n = 0;
-    }
-    
-    char *p = new char[n + 1];
-    
-    int i;
-    for (i = 0; i < n && str[i]; i++) {
-        p[i] = str[i];
-    }
-    while (i <= n) {
-        p[i++] = '\0';
-    }
-    
-    return p;
+template <typename T>
+void Swap(T &a, T &b) {
+    T temp;
+    temp = a;
+    a = b;
+    b = temp;
 }
