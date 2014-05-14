ef86683de35fe6166aaaee7eab0064633e74c9be
Sergey Alpeev <salpeev@GFL-1894.local>
2014-05-14 10:56:42 +0300

    Listing 8.7

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -13,79 +13,53 @@ using namespace std;
 
 
 
-struct free_throws {
-    string name;
-    int made;
-    int attempts;
-    float percent;
-};
-
-
-
-void display(const free_throws &ft);
-void set_pc(free_throws &ft);
-free_throws & accumulate(free_throws &target, const free_throws &source);
+string version1(const string &s1, const string &s2);
+const string & version2(string &s1, const string &s2);
+const string & version3(string &s1, const string &s2);
 
 
 
 int main() {
-    free_throws one = {"Ifelsa Branch", 13, 14};
-    free_throws two = {"Andor Knott", 10, 16};
-    free_throws three = {"Minnie Max", 7, 9};
-    free_throws four = {"Whily Looper", 5, 9};
-    free_throws five = {"Long Long", 6, 14};
-    free_throws team = {"Throwgoods", 0, 0};
+    string input;
+    string copy;
+    string result;
     
-    free_throws dup;
+    cout << "Enter a string: ";
+    getline(cin, input);
+    copy = input;
+    cout << "Your string as entered: " << input << endl;
+    result = version1(input, "***");
+    cout << "Your string enhanced: " << result << endl;
+    cout << "Your original string: " << input << endl;
     
-    set_pc(one);
-    display(one);
+    result = version2(input, "###");
+    cout << "Your string enhanced: " << result << endl;
+    cout << "Your original string: " << input << endl;
     
-    accumulate(team, one);
-    display(team);
-    
-    display(accumulate(team, two));
-    accumulate(accumulate(team, three), four);
-    display(team);
-    
-    dup = accumulate(team, five);
-    
-    cout << "Display team:\n";
-    display(team);
-    
-    cout << "Displaying dup after assignment:\n";
-    display(dup);
-    
-    set_pc(four);
-    
-    // ill-adviced assignment
-    accumulate(dup, five) = four;
-    cout << "Displaying dup after ill-adviced assignment:\n";
-    display(dup);
+    cout << "Resetting original string.\n";
+    input = copy;
+    result = version3(input, "@@@");
+    cout << "Your string enhanced: " << result << endl;
+    cout << "Your original string: " << input << endl;
     
     return 0;
 }
 
 
 
-void display(const free_throws &ft) {
-    cout << "Name: " << ft.name << endl;
-    cout << "  Made: " << ft.made;
-    cout << "\tAttempts: " << ft.attempts;
-    cout << "\tPercent: " << ft.percent << endl;
+string version1(const string &s1, const string &s2) {
+    string temp;
+    temp = s2 + s1 + s2;
+    return temp;
 }
 
-void set_pc(free_throws &ft) {
-    if (ft.attempts != 0) {
-        ft.percent = 100.0 * float(ft.made) / float(ft.attempts);
-    } else {
-        ft.percent = 0.0;
-    }
+const string & version2(string &s1, const string &s2) {
+    s1 = s2 + s1 + s2;
+    return s1;
 }
 
-free_throws & accumulate(free_throws &target, const free_throws &source) {
-    target.attempts += source.attempts;
-    target.made += source.made;
-    set_pc(target);
-    return target;
+const string & version3(string &s1, const string &s2) {
+    string temp;
+    temp = s2 + s1 + s2;
+    return temp;
 }
