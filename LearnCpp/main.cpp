e865da52786bf9538daa8f241811ceef29af0eea
Sergey Alpeev <salpeev@GFL-1894.local>
2014-05-15 16:50:19 +0300

    decltype()

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -12,31 +12,17 @@ using namespace std;
 
 
 
-template <class T>
-T lesser(T a, T b) {
-    cout << "#1: ";
-    return (a < b) ? a : b;
-}
-
-int lesser(int a, int b) {
-    cout << "#2: ";
-    a = (a < 0) ? -a : a;
-    b = (b < 0) ? -b : b;
-    return (a < b) ? a : b;
-}
-
-
-
 int main() {
-    int m = 20;
-    int n = -30;
-    double x = 15.5;
-    double y = 25.9;
+    int a = 4;
+    int b = 5;
     
-    cout << lesser(m, n) << endl;
-    cout << lesser(x, y) << endl;
-    cout << lesser<>(m, n) << endl;
-    cout << lesser<int>(x, y) << endl;
+    decltype(a) x = 3;
+    cout << x << endl;
+    
+    decltype((a)) y = b;
+    cout << y << endl;
+    y = 7;
+    cout << b << endl;
     
     return 0;
 }
