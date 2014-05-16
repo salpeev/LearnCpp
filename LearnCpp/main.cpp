90822557003479cfbf0bc002c6a9a544b375ea07
Sergey Alpeev <salpeev@GFL-1894.local>
2014-05-16 15:44:16 +0300

    Listing 9.9

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -12,20 +12,49 @@ using namespace std;
 
 
 
-int tom = 3;
-int dick = 30;
-static int harry = 300;
+const int ArSize = 10;
 
 
 
-void remote_access();
+void strcount(const char *str);
 
 
 
 int main() {
-    cout << "main() reports the following addresses:\n";
-    cout << &tom << " = &tom, " << &dick << " = &dick, " << &harry << " = &harry\n";
-    remote_access();
+    char input[ArSize];
+    char next;
+    
+    cout << "Enter a line: ";
+    cin.get(input, ArSize);
+    
+    while (cin) {
+        cin.get(next);
+        while (next != '\n') {
+            cin.get(next);
+        }
+        
+        strcount(input);
+        
+        cout << "Enter next line (empty line to quit): ";
+        cin.get(input, ArSize);
+    }
     
     return 0;
+}
+
+
+
+void strcount(const char *str) {
+    static int total = 0;
+    int count = 0;
+    
+    cout << "\"" << str << "\" contains ";
+    
+    while (*str++) {
+        count++;
+    }
+    total += count;
+    
+    cout << count << " characters\n";
+    cout << total << " total\n";
 }
