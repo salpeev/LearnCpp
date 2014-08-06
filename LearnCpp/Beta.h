86aaeaf9c8b68033f949d2072324109547ac619a
Sergey Alpeev <salpeev@briskmobile.com>
2014-08-06 14:52:12 +0300

    Listing 14.20 Updated

--- LearnCpp/Beta.h
+++ LearnCpp/Beta.h
@@ -22,24 +22,36 @@ public:
     Beta(T t, int i): q(t), n(i) {}
     
     template <typename U>
-    U blab(U u, T t) {return (n.Value() * q.Value()) * u / t;}
+    U blab(U u, T t);
     void Show() const {q.Show(); n.Show();}
     
 private:
     template <typename V>
-    class Hold {
-    public:
-        Hold(V v = 0): val(v) {}
-        
-        void Show() const {cout << val << endl;}
-        V Value() const {return val;}
-        
-    private:
-        V val;
-    };
+    class Hold;
     
     Hold<T> q;
     Hold<int> n;
 };
+
+
+
+template <typename T>
+template <typename V>
+class Beta<T>::Hold {
+public:
+    Hold(V v = 0): val(v) {}
+    
+    void Show() const {cout << val << endl;}
+    V Value() const {return val;}
+    
+private:
+    V val;
+};
+
+template <typename T>
+template <typename U>
+U Beta<T>::blab(U u, T t) {
+    return (n.Value() * q.Value()) * u / t;
+}
 
 #endif
