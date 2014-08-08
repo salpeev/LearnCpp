856cd2705deb77bccdbfeea381ef20a4120c0651
Sergey Alpeev <salpeev@briskmobile.com>
2014-08-08 11:53:38 +0300

    Listing 15.5, 15.6

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -7,35 +7,31 @@
 //
 
 #include <iostream>
-#include "Tv.h"
+#include <string>
+#include "Queue.h"
 
 
 
 int main() {
+    using std::string;
     using std::cout;
+    using std::cin;
     
-    Tv s42;
+    Queue<string> cs(5);
+    string temp;
     
-    cout << "Initial settings for 42\" TV:\n";
-    s42.settings();
-    s42.onoff();
-    s42.chanup();
-    cout << "Adjusted settings for 42\" TV:\n";
-    s42.settings();
+    while (!cs.isfull()) {
+        cout << "Please enter your name. You will be served in the order of arrival.\nname: ";
+        getline(cin, temp);
+        cs.enqueue(temp);
+    }
     
-    Remote grey;
+    cout << "The queue is full. Processing begins.\n";
     
-    grey.set_chan(s42, 10);
-    grey.volup(s42);
-    grey.volup(s42);
-    cout << "\n42\" settings after using remote:\n";
-    s42.settings();
-    
-    Tv s58(Tv::On);
-    s58.set_mode();
-    grey.set_chan(s58, 28);
-    cout << "\n58\" settings:\n";
-    s58.settings();
+    while (!cs.isempty()) {
+        cs.dequeue(temp);
+        cout << "Now processing " << temp << "...\n";
+    }
     
     return 0;
 }
